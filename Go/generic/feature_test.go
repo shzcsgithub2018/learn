40f160897c04b94ffc3b4a8e88ef4b05a190e667
songhaizhou <songhaizhou@bytedance.com>
2022-06-23 00:49:50 +0800

    generic test

---
+++ Go/generic/feature_test.go
@@ -0,0 +1,28 @@
+package generic
+
+import (
+	"testing"
+)
+
+
+func TestMuitType(t *testing.T) {
+	a := 1
+	b := 2
+	t.Log(GetMaxNum(a, b))
+
+
+	c := "123"
+	d := "456"
+	t.Log(GetMaxNum(c, d))
+
+	e := int64(100)
+	f := int64(0)
+	t.Log(GetMaxNum(e, f))
+}
+
+func GetMaxNum[T any](a, b T) T {
+	if a >= b {
+		return a
+	}
+	return b
+}
