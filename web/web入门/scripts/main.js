f1750c3670a76e49bf23449f057302fd8b81ddd5
Haizhou <1919062854@qq.com>
2025-01-15 10:08:04 +0800

    save

---
+++ web/web入门/scripts/main.js
@@ -0,0 +1,7 @@
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!";
+
+
+document.querySelector("html").addEventListener("click", function () {
+    alert("别戳我，我怕疼！");
+  });
