c52cd46f96132b8c8f60d15d35e5c8490278edf9
Haizhou <1919062854@qq.com>
2025-01-16 09:41:22 +0800

    save

--- web/web入门/scripts/main.js
+++ web/web入门/scripts/main.js
@@ -1,7 +1,37 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
+const myImage = document.querySelector("img");
 
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/firefox.png") {
+    myImage.setAttribute("src", "images/firefox2.png");
+  } else {
+    myImage.setAttribute("src", "images/firefox.png");
+  }
+};
 
-document.querySelector("html").addEventListener("click", function () {
-    alert("别戳我，我怕疼！");
-  });
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if (!myName) {
+        setUserName();
+    } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+    }
+    // localStorage.setItem("name", myName);
+    // myHeading.textContent = `Mozilla is cool, ${myName}`;
+}
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Mozilla is cool, ${storedName}`;
+  }
+
+  myButton.onclick = function () {
+    setUserName();
+  };
+  
